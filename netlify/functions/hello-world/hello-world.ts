87801ecfdd649d3578f81efe284a49c30ea63f60
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2022-04-01 12:30:08 -0300

    probando 2

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,36 +1,3 @@
-// import { Handler } from "@netlify/functions";
-// import chromium from "chrome-aws-lambda";
-
-// export const handler: Handler = async (event, context) => {
-//   const { name = "stranger" } = event.queryStringParameters;
-
-//   const browser = await chromium.puppeteer.launch({
-//     executablePath: await chromium.executablePath,
-//     defaultViewport: {
-//       width: 1200,
-//       height: 675,
-//     },
-//     headless: true,
-//   });
-
-//   const page = await browser.newPage();
-
-//   await page.setContent(`
-//     <h1>Hola ${name} hoy es ${new Date().toLocaleTimeString()}</h1>
-//   `);
-
-//   const buffer = await page.screenshot();
-
-//   return {
-//     statusCode: 200,
-//     headers: {
-//       "Content-Type": "image/png",
-//     },
-//     body: buffer.toString("base64"),
-//     isBase64Encoded: true,
-//   };
-// };
-
 import { Handler } from "@netlify/functions";
 const chromium = require("chrome-aws-lambda");
 
@@ -66,7 +33,7 @@ export const handler: Handler = async (event, context) => {
     return {
       statusCode: 200,
       headers: {
-        "Content-Type": "image/png",
+        "Content-Type": "text/html",
         body: JSON.stringify({
           error: error.message,
         }),
