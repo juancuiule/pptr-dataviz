d0d7221d1f90c3923716aeb0a57e10979c4afb61
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2022-04-01 12:45:04 -0300

    probando 3

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,43 +1,33 @@
 import { Handler } from "@netlify/functions";
-const chromium = require("chrome-aws-lambda");
+import chromium from "chrome-aws-lambda";
 
 export const handler: Handler = async (event, context) => {
   const { name = "stranger" } = event.queryStringParameters;
 
-  try {
-    const browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: {
+      width: 1200,
+      height: 675,
+    },
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+  });
 
-    let page = await browser.newPage();
+  const page = await browser.newPage();
 
-    await page.setContent(`
+  await page.setContent(`
     <h1>Hola ${name} hoy es ${new Date().toLocaleTimeString()}</h1>
   `);
 
-    const buffer = await page.screenshot();
+  const buffer = await page.screenshot();
 
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "image/png",
-      },
-      body: buffer.toString("base64"),
-      isBase64Encoded: true,
-    };
-  } catch (error) {
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "text/html",
-        body: JSON.stringify({
-          error: error.message,
-        }),
-      },
-    };
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "image/png",
+    },
+    body: buffer.toString("base64"),
+    isBase64Encoded: true,
+  };
 };
