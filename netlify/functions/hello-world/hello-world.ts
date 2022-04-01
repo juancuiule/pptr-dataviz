c2b87aa158c45d3f138adbb2d4c9b84023786cab
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2022-04-01 11:56:22 -0300

    function

---
+++ netlify/functions/hello-world/hello-world.ts
@@ -0,0 +1,33 @@
+import { Handler } from '@netlify/functions'
+import puppeteer from 'puppeteer-core'
+import chromium from 'chrome-aws-lambda'
+
+export const handler: Handler = async (event, context) => {
+  const { name = 'stranger' } = event.queryStringParameters
+
+  const browser = await puppeteer.launch({
+    executablePath: await chromium.executablePath,,
+    args: chromium.args,
+    defaultViewport: {
+      width: 1200,
+      height: 675,
+    }
+  })
+
+  const page = await browser.newPage()
+
+  await page.setContent(`
+    <h1>Hola ${name} hoy es ${new Date().toLocaleTimeString()}</h1>
+  `);
+
+  const buffer = await page.screenshot()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'image/png',
+    },
+    body: buffer.toString('base64'),
+    isBase64Encoded: true
+  }
+}
